8f76ab07fbebf61bc41ea5d1472798a5ac4fb4b3
Chang Liu <wewe.chang@gmail.com>
2021-08-04 15:45:29 -0700

    add linkedin github link

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import { FaGithub, FaLinkedinIn } from "react-icons/fa"
+
 import Head from 'next/head'
 import Image from 'next/image'
 
@@ -9,16 +11,26 @@ export default function Home() {
         <meta name="description" content="Chang Liu, full stack engineer" />
       </Head>
       <div className="container flex flex-col md:flex-row mx-auto h-screen justify-center items-center">
-        <div className="bg-gray-100 rounded-full p-2 flex items-center justify-center w-auto">
-          <Image className="rounded-full" src="/images/profile.jpg" alt="profile" width={150} height={150} />
+        <div className="flex flex-col items-center flex-shrink-0">
+          <div className="bg-gray-100 rounded-full p-2 flex items-center justify-center w-auto flex-shrink-0">
+            <Image className="rounded-full" src="/images/profile.jpg" alt="profile" width={150} height={150} />
+          </div>
+          <div className="flex items-center my-2">
+            <a href="https://www.linkedin.com/in/chang-liu-87038691/" className="mx-2">
+              <FaLinkedinIn className="text-gray-400" />
+            </a>
+            <a href="https://github.com/chngl">
+              <FaGithub className="text-gray-400" />
+            </a>
+          </div>
         </div>
         <div className="mx-2 flex flex-col items-center md:items-start">
           <div className="text-4xl">
             Chang Liu
           </div>
           <div className="text-gray-500 my-8">
-            Hello world! This is Chang, a full stack engineer who has a passion to create end to end solution to solve problems at scale.
-        </div>
+            {"Hello world! This is Chang, a full stack software engineer who has a passion to create end to end solution to solve problems at scale."}
+          </div>
         </div>
       </div>
     </div>
