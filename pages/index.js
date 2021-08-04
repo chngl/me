8e9e5d62208d6e2cf127f975f1a346c8c1924003
Chang Liu <wewe.chang@gmail.com>
2021-08-04 10:26:56 -0700

    make the page responsive

--- pages/index.js
+++ pages/index.js
@@ -8,16 +8,16 @@ export default function Home() {
         <title>Chang</title>
         <meta name="description" content="Chang Liu, full stack engineer" />
       </Head>
-      <div className="container flex mx-auto h-screen justify-center items-center">
+      <div className="container flex flex-col md:flex-row mx-auto h-screen justify-center items-center">
         <div className="bg-gray-100 rounded-full p-2 flex items-center justify-center w-auto">
           <Image className="rounded-full" src="/images/profile.jpg" alt="profile" width={150} height={150} />
         </div>
-        <div className="mx-2">
+        <div className="mx-2 flex flex-col items-center md:items-start">
           <div className="text-4xl">
             Chang Liu
           </div>
           <div className="text-gray-500 my-8">
-            Hello world! This is Chang, a full stack engineer who has a passion to create end to end solutions to solve problems at scale.
+            Hello world! This is Chang, a full stack engineer who has a passion to create end to end solution to solve problems at scale.
         </div>
         </div>
       </div>
