e053828e8b4a96104e23e78ad44b84278cb26e7a
Chang Liu <it@til.xyz>
2024-04-08 13:49:27 -0700

    revive my site

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-const withTM = require('next-transpile-modules')(['@chngl/jellojs']);
-
-module.exports = withTM({
-  reactStrictMode: true,
-  images: {
-    domains: ['dl.airtable.com', 'drive.google.com'],
-  },
-});
+module.exports = {
+    reactStrictMode: true,
+    images: {
+        domains: ["dl.airtable.com", "drive.google.com"],
+    },
+    transpilePackages: ["@chngl/jellojs"],
+};
